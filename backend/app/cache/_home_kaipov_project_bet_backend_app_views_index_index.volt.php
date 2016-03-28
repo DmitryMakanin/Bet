1990be38c7b97fdde14957e36fbedca2b8130d0e
Kaipov <kaipov1995@gmail.com>
2016-03-28 23:29:44 +0300

    fixed index.volt

--- backend/app/cache/_home_kaipov_project_bet_backend_app_views_index_index.volt.php
+++ backend/app/cache/_home_kaipov_project_bet_backend_app_views_index_index.volt.php
@@ -6,10 +6,16 @@
   <li class="active">How to implement this?</li>
 </ul>
 
+<blockquote class="blockquote-reverse">
+  <p>Бабки не проблема!</p>
+  <small>Женя Часовитин</small>
+</blockquote>
+
 <ul class="nav">
 	<li><a href="/sportkind/">Установки видов спорта</a></li>
 	<li><a href="/country/">Установки стран</a></li>
-	<li><a href="/team/">Установки команд</a></li>	
 	<li><a href="/league/">Установки лиг</a></li>
+	<li><a href="/season/">Установки сезонов</a></li>
+	<li><a href="/team/">Установки команд</a></li>
 	<li><a href="/parametrs/">Установки параметров</a></li>
 </ul>
